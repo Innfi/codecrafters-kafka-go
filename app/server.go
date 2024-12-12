ebfef772fb86301a1e4fced1d496c817a547ea72
innfi <innfi.world@gmail.com>
2024-12-12 23:26:33 +0900

    parsing api versions

--- app/server.go
+++ app/server.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-	// "encoding/binary"
+	"encoding/binary"
 	"fmt"
 	"net"
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
+type Message struct {
+	MessageSize       []byte
+	RequestApiKey     []byte
+	RequestApiVersion []byte
+	CorellationId     []byte
+}
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
 
 	l, err := net.Listen("tcp", "0.0.0.0:9092")
 	if err != nil {
@@ -30,27 +30,27 @@ func main() {
 	defer conn.Close()
 
 	buf := make([]byte, 100)
-	readLen, readErr := conn.Read(buf)
+	_, readErr := conn.Read(buf)
 	if readErr != nil {
 		fmt.Println("read error: ", readErr.Error())
 		os.Exit(1)
 	}
 
-	printBuf(buf, readLen)
+	message := toMessage(buf)
+	printBuf(message.MessageSize, 4)
+	printBuf(message.RequestApiKey, 2)
+	printBuf(message.RequestApiVersion, 2)
+	printBuf(message.CorellationId, 4)
 
-	outBuf := make([]byte, 8)
-	// binary.BigEndian.PutUint32(outBuf[4:], 0x07)
-	copy(outBuf[4:8], buf[8:12])
+	requestApiVersion := binary.BigEndian.Uint16(message.RequestApiVersion)
+	fmt.Printf("requestApiVersion: %d\n", requestApiVersion)
 
-	for _, elem := range outBuf {
-		fmt.Printf("%02x ", elem)
+	if requestApiVersion > 4 {
+		sendErrorResponse(conn, message)
+		os.Exit(0)
 	}
-	fmt.Printf("\n")
 
-	_, writeErr := conn.Write(outBuf)
-	if writeErr != nil {
-		fmt.Println("write error: ", writeErr.Error())
-	}
+	sendOkResponse(conn, message)
 }
 
 func printBuf(buf []byte, readLen int) {
@@ -58,7 +58,49 @@ func printBuf(buf []byte, readLen int) {
 		if index >= readLen {
 			break
 		}
-		fmt.Printf("%02x", elem)
+		fmt.Printf("%02x ", elem)
 	}
 	fmt.Printf("\n")
+}
+
+func toMessage(buf []byte) Message {
+	for index, elem := range buf {
+		if index > 13 {
+			break
+		}
+		fmt.Printf("index: %d, elem: %02x\n", index, elem)
+	}
+
+	return Message{
+		MessageSize:       buf[0:4],
+		RequestApiKey:     buf[4:6],
+		RequestApiVersion: buf[6:8],
+		CorellationId:     buf[8:12],
+	}
+}
+
+func sendErrorResponse(conn net.Conn, message Message) {
+	outBuf := make([]byte, 10)
+	copy(outBuf[4:8], message.CorellationId)
+	outBuf[8] = 0x00
+	outBuf[9] = 0x23
+
+	printBuf(outBuf, len(outBuf))
+
+	_, writeErr := conn.Write(outBuf)
+	if writeErr != nil {
+		fmt.Println("write error: ", writeErr.Error())
+	}
+}
+
+func sendOkResponse(conn net.Conn, message Message) {
+	outBuf := make([]byte, 8)
+	copy(outBuf[4:8], message.CorellationId)
+
+	printBuf(outBuf, len(outBuf))
+
+	_, writeErr := conn.Write(outBuf)
+	if writeErr != nil {
+		fmt.Println("write error: ", writeErr.Error())
+	}
 }
