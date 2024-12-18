e76995e892a6a5f4b019e967ef4ed7a4bf0f419a
innfi <innfi.world@gmail.com>
2024-12-18 23:27:00 +0900

    finding ways to convert struct to hex array

--- init_test.go
+++ init_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -27,4 +28,38 @@ func TestAssembleBytes(t *testing.T) {
 
 	output = append(output, input...)
 	assert.Equal(t, len(output), 8)
+}
+
+type ApiVersionSpec struct {
+	ApiKey        uint16
+	ApiVersionMin uint16
+	ApiVersionMax uint16
+}
+
+func StructToBytes(obj interface{}) ([]byte, error) {
+	buf := new(bytes.Buffer)
+	err := binary.Write(buf, binary.BigEndian, obj)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func TestStructToByteArray(t *testing.T) {
+	versionSpec := ApiVersionSpec{
+		ApiKey:        18,
+		ApiVersionMin: 0,
+		ApiVersionMax: 4,
+	}
+
+	expectedResult := []byte{0x00, 0x12, 0x00, 0x00, 0x00, 0x04}
+
+	byteArray, _ := StructToBytes(versionSpec)
+
+	// not working as expected
+	assert.Equal(t, len(expectedResult), len(byteArray))
+
+	for i := 0; i < len(expectedResult); i++ {
+		assert.Equal(t, expectedResult[i], byteArray[i])
+	}
 }
